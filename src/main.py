a3ee300a19412e191b5a824a45f5a31544e86669
ok_kir <clicktmin@gmail.com>
2023-01-28 16:20:22 +0400

    feat: Add route for root url

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 _DESCRIPTION = """
 Пример решений задания для Backend-разработчика Школы IT 🚀.
@@ -14,6 +15,26 @@ app = FastAPI(title="task-backend--example",
               description=_DESCRIPTION,
               version="1.0.0",
               redoc_url=None)
+
+
+@app.get("/", response_class=HTMLResponse, tags=["Main"])
+async def root():
+    """Обработчик  **ГЛАВНОГО** пути"""
+    html_content = """
+    <html>
+        <head>
+            <title>task-backend--example</title>
+        </head>
+        <body>
+            <h1>Пример решения задания для Backend-разработчика Школы IT</h1>
+            <div>
+                ✍ <a href="/docs">Документация приведена по данному адресу</a> 
+            </div>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content)
+
 
 if __name__ == '__main__':
     import uvicorn
