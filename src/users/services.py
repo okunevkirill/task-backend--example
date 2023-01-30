eb4401e814e2576d29763e20e4ff530199d2f699
ok_kir <clicktmin@gmail.com>
2023-01-30 16:04:34 +0400

    feat: Add services for 'User'

---
+++ src/users/services.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import Session
+
+from .models import User
+from .schemas import UserInputSchema
+
+
+def get_users(session: Session, skip: int = 0, limit: int = 100, gender=None):
+    if gender:
+        return session.query(User).filter_by(gender=gender).offset(skip).limit(limit)
+    return session.query(User).offset(skip).limit(limit).all()
+
+
+def add_user(session: Session, data: UserInputSchema):
+    user = User(**data.dict())
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    return user
