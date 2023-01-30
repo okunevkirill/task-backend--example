06861766a7d1c8ebbe5ab83409b6f12f36082ed9
ok_kir <clicktmin@gmail.com>
2023-01-30 20:16:22 +0400

    style: Change quotes ("__main__":)

--- src/main.py
+++ src/main.py
@@ -43,7 +43,7 @@ async def root():
 # Регистрация маршрутов от функциональных модулей
 app.include_router(router)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(app)
